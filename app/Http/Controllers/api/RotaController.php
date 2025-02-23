144956007a49e696a456dabed29cef56cf692e3d
andrew dylan <andrewdylanvianaamorim.2@gmail.com>
2025-02-23 12:32:09 -0300

    pequena correção

--- app/Http/Controllers/api/RotaController.php
+++ app/Http/Controllers/api/RotaController.php
@@ -14,7 +14,7 @@ class RotaController
         $query->where('rota.ativo', '=', true);
         $query->join('veiculo', 'veiculo.placa', '=', 'rota.placa');
         $query->join('motorista', 'motorista.cnh', '=', 'rota.motorista');
-        return $query->get(['id', 'data', 'veiculo.placa', 'motorista.nome as motorista', 'origem', 'destino', 'distancia', 'pesoCarga', 'receita', 'combustivel', 'pedagio', 'outros', 'noPrazo', 'rota.ativo', 'motorista.id as motoristaid']);
+        return $query->get(['id', 'data', 'veiculo.placa', 'motorista.nome as motorista', 'origem', 'destino', 'distancia', 'pesoCarga', 'receita', 'combustivel', 'pedagio', 'outros', 'noPrazo', 'rota.ativo', 'motorista.cnh as cnhmotorista']);
     }
 
     
@@ -45,7 +45,7 @@ class RotaController
         $query->where('id', '=', $id);
         $query->join('veiculo', 'veiculo.placa', '=', 'rota.placa');
         $query->join('motorista', 'motorista.cnh', '=', 'rota.motorista');
-        return $query->get(['id', 'data', 'veiculo.placa', 'motorista.nome as motorista', 'origem', 'destino', 'distancia', 'pesoCarga', 'receita', 'combustivel', 'pedagio', 'outros', 'rota.ativo', 'noPrazo', 'motorista.id as motoristaid']);
+        return $query->get(['id', 'data', 'veiculo.placa', 'motorista.nome as motorista', 'origem', 'destino', 'distancia', 'pesoCarga', 'receita', 'combustivel', 'pedagio', 'outros', 'rota.ativo', 'noPrazo', 'motorista.cnh as cnhmotorista']);
     }
 
     
